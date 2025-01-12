8d21baf3f50c866ee4edf1762c9c699c0dc3cb78
WoozyMasta <kagbe.leviy@gmail.com>
2025-01-12 06:22:26 +0300

    feat: extend serverlist tests

--- serverlist/serverlist_test.go
+++ serverlist/serverlist_test.go
@@ -4,31 +4,85 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/woozymasta/steam/utils/appid"
 )
 
-func TestGetServers(t *testing.T) {
-	key, ok := os.LookupEnv("STEAM_API_KEY")
-	if !ok {
-		t.Error("Steam API key must be pass in variable 'STEAM_API_KEY'")
-	}
-
-	limit := 1000
-
-	query := New(key)
-	query.SetLimit(limit)
+// Return Counter-Strike 2 servers
+func TestGetCS2Servers(t *testing.T) {
+	var id uint64 = appid.CounterStrike2.Uint64()
 
 	filter := &Filter{}
-	filter.Add(KeyAppID, "221100")
+	filter.Add(KeyAppID, fmt.Sprintf("%d", id))
+	filter.Add(KeyMap, "de_dust2")
+	filter.AddNor(KeyGameType, "insecure")
+	filter.AddNor(KeyGameType, "empty")
+
+	if err := helperGetServers(id, filter, 50); err != nil {
+		t.Error(err)
+	}
+}
+
+// Return Counter-Strike Source servers
+func TestGetCSSServers(t *testing.T) {
+	var id uint64 = appid.CounterStrikeSource.Uint64()
+
+	filter := &Filter{}
+	filter.Add(KeyAppID, fmt.Sprintf("%d", id))
+	filter.Add(KeyMap, "de_dust2")
+
+	if err := helperGetServers(id, filter, 50); err != nil {
+		t.Error(err)
+	}
+}
+
+// Return official servers of DayZ
+func TestGetDayZServers(t *testing.T) {
+	var id uint64 = appid.DayZ.Uint64()
+
+	filter := &Filter{}
+	filter.Add(KeyAppID, fmt.Sprintf("%d", id))
 	filter.Add(KeyGameType, "battleye")
 	filter.AddNor(KeyGameType, "external")
 
+	if err := helperGetServers(id, filter, 200); err != nil {
+		t.Error(err)
+	}
+}
+
+// Return official servers of Arma3
+func TestGetArma3Servers(t *testing.T) {
+	var id uint64 = appid.Arma3.Uint64()
+
+	filter := &Filter{}
+	filter.Add(KeyAppID, fmt.Sprintf("%d", id))
+	filter.Add(KeyName, "\xb6 [ OFFICIAL ] Arma 3 *")
+	filter.Add(KeyGameType, "bt")
+	filter.Add(KeyGameType, "dt")
+
+	if err := helperGetServers(id, filter, 150); err != nil {
+		t.Error(err)
+	}
+}
+
+func helperGetServers(appId uint64, filter *Filter, limit int) error {
+	key, ok := os.LookupEnv("STEAM_API_KEY")
+	if !ok {
+		return fmt.Errorf("Steam API key must be pass in variable 'STEAM_API_KEY'")
+	}
+
+	query := New(key)
+	query.SetLimit(limit)
 	servers, err := query.Get(filter)
 
 	if err != nil {
-		t.Errorf("Cant get files %v", err)
+		return fmt.Errorf("Cant get files %s", err)
 	}
 	if len(servers) > limit {
-		t.Errorf("Return %d servers, but expected %d by limit", len(servers), limit)
+		return fmt.Errorf("Return %d servers, but expected %d by limit", len(servers), limit)
+	}
+	if len(servers) == 0 {
+		return fmt.Errorf("Return 0 servers, but expected up to %d by limit", limit)
 	}
 
 	for _, s := range servers {
@@ -37,12 +91,16 @@ func TestGetServers(t *testing.T) {
 			s.GameDir, s.Addr, s.Name, s.Map, s.Players, s.MaxPlayers,
 		)
 
-		if s.Appid != 221100 {
-			t.Errorf("Return %d (%s) game, but expected %d", s.Appid, s.GameDir, 221100)
+		if s.Appid != appId {
+			return fmt.Errorf("Return %d (%s) game, but expected %d", s.Appid, s.GameDir, appId)
 		}
+
+		fmt.Printf("Keywords: %s\n", s.GameType)
 	}
 
 	for version, count := range servers.GetVersionMap() {
 		fmt.Printf("Version %s used on %d servers\n", version, count)
 	}
+
+	return nil
 }
